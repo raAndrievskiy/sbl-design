efabe7048279c4e2cf5f99be6a08bb594f882f4c
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-03-13 19:32:40 +0300

    show circle

--- src/pages/Main/components/BubblesInfo/BubblesInfo.tsx
+++ src/pages/Main/components/BubblesInfo/BubblesInfo.tsx
@@ -13,24 +13,12 @@ export const BubblesInfo: React.FC = () => {
   return (
     <div className={styles.bubbles}>
       <Container>
-        <span className="description">
+        <span className={`description ${styles.bubblesDescription}`}>
           Разрабатывая подобные идеи ты входишь в них с головой и со всем
           вниманием к процессу не ожидая ничего взамен.
         </span>
-        <div className={styles.bubblesIconWrap}>
-          {/* <Tween
-            to={{
-              // y: '10%',
-              // paddingBottom: '100px',
-              scrollTrigger: {
-                start: '1300vh 100vh',
-                end: '100% 100%',
-                scrub: true,
-                // markers: true,
-              },
-            }}> */}
+        <div className={`${styles.bubblesIconWrap} bubblesTrigger`}>
           <BubblesIcon className={styles.bubblesIcon} />
-          {/* </Tween> */}
           <Tween
             to={{
               scale: 2.5,
@@ -38,7 +26,7 @@ export const BubblesInfo: React.FC = () => {
               scrollTrigger: {
                 start: '1000vh 100vh',
                 end: '100% 100%',
-                scrub: true,
+                scrub: 2,
                 // markers: true,
               },
             }}>
