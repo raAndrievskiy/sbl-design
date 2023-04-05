d8e0ab8a5f8122b336aaf663721692e849fa72f3
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-04-05 19:55:05 +0300

    test scroll smoth in prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { Footer, Header, Preloader } from './components'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 
+import { Footer, Header, Preloader } from './components'
 import { Main, Premium, Special, About } from './pages'
 
-const App = () => {
+gsap.registerPlugin(ScrollTrigger)
+
+const App: React.FC = () => {
+  const appRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const app = appRef.current
+
+    gsap.to(app, {
+      scrollTrigger: {
+        trigger: app,
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: 60,
+        markers: true,
+      },
+    })
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/premium" element={<Premium />} />
-        <Route path="/special" element={<Special />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/preloader" element={<Preloader />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div ref={appRef}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/premium" element={<Premium />} />
+          <Route path="/special" element={<Special />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/preloader" element={<Preloader />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
