20f15bddc3bf01da3963fccd55032d3fa154cde8
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-04-07 14:03:27 +0300

    create ImageBackground component for special and premium page

--- src/components/ImageBackground/ImageBackground.tsx
+++ src/components/ImageBackground/ImageBackground.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { gsap } from 'gsap'
+import { Tween, ScrollTrigger } from 'react-gsap'
 
+import elevatorMain from '../../assets/images/special/specialBg.png'
 import styles from './styles.module.scss'
+
+gsap.registerPlugin(ScrollTrigger)
 
 interface ImageBackgroundTypes {
   text: string
@@ -8,12 +13,31 @@ interface ImageBackgroundTypes {
 
 export const ImageBackground: React.FC<ImageBackgroundTypes> = ({ text }) => {
   return (
-    <div className={styles.typeElevator}>
-      <div className={styles.typeElevatorMain}>
+    <div className={`${styles.typeElevator} typeElevatorTrigger`}>
+      <Tween
+        from={{
+          width: '20vw',
+        }}
+        to={{
+          width: '100vw',
+        }}
+        duration={1.5}>
+        <img
+          src={elevatorMain}
+          alt=""
+          className={`${styles.typeElevatorMain} typeElevatorMainTrigger`}
+        />
+      </Tween>
+      <Tween
+        from={{ left: '101%' }}
+        to={{
+          left: '50%',
+        }}
+        duration={1.5}>
         <span className={`${styles.typeElevatorMainText} color-white`}>
           SBL DESIGN – {text}
         </span>
-      </div>
+      </Tween>
     </div>
   )
 }
