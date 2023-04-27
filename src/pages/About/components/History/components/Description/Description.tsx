09e2a38f043574f2754f8c00b2bec526cbbebf32
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-04-27 19:07:50 +0300

    check deploy

--- src/pages/About/components/History/components/Description/Description.tsx
+++ src/pages/About/components/History/components/Description/Description.tsx
@@ -7,85 +7,112 @@ import styles from './styles.module.scss'
 gsap.registerPlugin(ScrollTrigger)
 
 export const Description: React.FC = () => {
-  const textContainer = useRef<HTMLDivElement>(null)
-  const textRef1 = useRef<HTMLDivElement>(null)
-  const textRef2 = useRef<HTMLDivElement>(null)
-  const textRef3 = useRef<HTMLDivElement>(null)
+  const textWrapRef = useRef<HTMLDivElement>(null)
+  const textRef1 = useRef<HTMLSpanElement>(null)
+  const textRef2 = useRef<HTMLSpanElement>(null)
+  const textRef3 = useRef<HTMLSpanElement>(null)
 
-  // useEffect(() => {
-  //   const tlText1 = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: textRef1.current,
-  //       start: 'top center-=50vh',
-  //       end: 'bottom-=50vh center',
-  //       scrub: 0.5,
-  //     },
-  //   })
-  //   const tlText2 = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: textRef2.current,
-  //       start: 'top-=50v center-=50vh',
-  //       end: 'bottom center',
-  //       scrub: 0.5,
-  //     },
-  //   })
-  //   const tlText3 = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: textRef3.current,
-  //       start: 'top center-=50vh',
-  //       end: 'bottom center',
-  //       // end: 'bottom-=10vh center',
-  //       scrub: 0.5,
-  //       // markers: true,
-  //     },
-  //   })
+  useEffect(() => {
+    const textWrap = textWrapRef.current
+    const text1 = textRef1.current
+    const text2 = textRef2.current
+    const text3 = textRef3.current
 
-  //   tlText1.fromTo(textRef1.current, { y: 0 }, { y: 0 })
-  //   tlText1.to(textRef1.current, { opacity: 0, y: -100 }, '+=1')
+    const tlText1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: text1,
+        start: 'bottom+=300vh center',
+        end: 'bottom+=300vh center',
+        // markers: true,
+      },
+    })
 
-  //   tlText2.fromTo(
-  //     textRef2.current,
-  //     { opacity: 0, y: 0 },
-  //     { opacity: 1, y: -250 },
-  //   )
-  //   tlText2.to(textRef2.current, { opacity: 0, y: -350 }, '+=1')
+    tlText1.fromTo(
+      text1,
+      {
+        top: 0,
+        opacity: 1,
+      },
+      {
+        position: 'fixed',
+        top: '-40px',
+        opacity: 0,
+      },
+    )
 
-  //   tlText3.fromTo(
-  //     textRef3.current,
-  //     { opacity: 0, y: 0 },
-  //     { opacity: 1, y: -470 },
-  //   )
+    gsap.to(text1, {
+      position: 'fixed',
+      scrollTrigger: {
+        trigger: textWrap,
+        start: 'top-=350vh top',
+        end: 'bottom center',
+        scrub: true,
+        markers: true,
+      },
+      onComplete: () => {
+        // text1.style.position = 'absolute'
+        // subtitle.style.left = '0px'
+      },
+    })
 
-  //   return () => {
-  //     tlText1.kill()
-  //     tlText2.kill()
-  //     tlText3.kill()
-  //   }
-  // }, [])
+    // const tlText2 = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '.elevatorTrigger2',
+    //     start: 'top-=50v center-=50vh',
+    //     end: 'bottom center',
+    //     scrub: 0.5,
+    //   },
+    // })
+    // const tlText3 = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '.elevatorTrigger3',
+    //     start: 'top center-=50vh',
+    //     end: 'bottom center',
+    //     // end: 'bottom-=10vh center',
+    //     scrub: 0.5,
+    //     // markers: true,
+    //   },
+    // })
+
+    // tlText2.fromTo(
+    //   textRef2.current,
+    //   { opacity: 0, y: 0 },
+    //   { opacity: 1, y: -250 },
+    // )
+    // tlText2.to(textRef2.current, { opacity: 0, y: -350 }, '+=1')
+
+    // tlText3.fromTo(
+    //   textRef3.current,
+    //   { opacity: 0, y: 0 },
+    //   { opacity: 1, y: -470 },
+    // )
+
+    // return () => {
+    //   tlText1.kill()
+    // tlText2.kill()
+    // tlText3.kill()
+    // }
+  }, [])
 
   return (
-    <div className="text-blocks-container" ref={textContainer}>
-      <div className="text-block" ref={textRef1}>
-        <p className={styles.imgDescription}>
-          Разрабатывая подобные идеи ты входишь в них с головой и со всем
-          вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
-          Разрабатывая подобные идеи.
-        </p>
-      </div>
-      <div className="text-block" ref={textRef2}>
-        <p className={styles.imgDescription}>
-          Разрабатывая подобные идеи ты входишь в них с головой и со всем
-          вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
-          Разрабатывая подобные идеи.
-        </p>
-      </div>
-      <div className="text-block" ref={textRef3}>
-        <p className={styles.imgDescription}>
-          Разрабатывая подобные идеи ты входишь в них с головой и со всем
-          вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
-          Разрабатывая подобные идеи.
-        </p>
-      </div>
+    <div className={styles.imgDescriptionWrap} ref={textWrapRef}>
+      <span className={styles.imgDescription} ref={textRef1}>
+        Разрабатывая подобные идеи ты входишь в них с головой и со всем
+        вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
+        Разрабатывая подобные идеи.
+      </span>
+
+      {/* <span className={styles.imgDescription} ref={textRef2}>
+        Разрабатывая подобные идеи ты входишь в них с головой и со всем
+        вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
+        Разрабатывая подобные идеи.
+      </span>
+
+      <span className={styles.imgDescription} ref={textRef3}>
+        Разрабатывая подобные идеи ты входишь в них с головой и со всем
+        вниманием к процессу не ожидая ничего взамен. Разрабатывая подобные.
+        Разрабатывая подобные идеи.
+      </span> */}
     </div>
   )
 }
--- src/pages/About/components/History/components/Images/Images.tsx
+++ src/pages/About/components/History/components/Images/Images.tsx
@@ -1,55 +1,26 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 
-import mockImg from '../../../../../../assets/images/mockImg.png'
+import historyImg01 from '../../../../../../assets/images/about/historyImg01.png'
+import historyImg02 from '../../../../../../assets/images/about/historyImg02.png'
+import historyImg03 from '../../../../../../assets/images/about/historyImg03.png'
 import styles from './styles.module.scss'
 
 gsap.registerPlugin(ScrollTrigger)
 
 export const Images: React.FC = () => {
-  const imageRef1 = useRef<HTMLDivElement>(null)
-
-  // useEffect(() => {
-  //   const tl = gsap.timeline({
-  //     scrollTrigger: {
-  //       // trigger: '.historyTrigger',
-  //       trigger: imageRef1.current,
-  //       start: 'top-=340vh top',
-  //       end: 'bottom-=900vh bottom',
-  //       scrub: 0.5,
-  //       // markers: true,
-  //     },
-  //   })
-
-  //   tl.fromTo(imageRef1.current, { y: 0 }, { y: '-75%' })
-
-  //   return () => {
-  //     tl.kill()
-  //     if (tl.scrollTrigger) {
-  //       tl.scrollTrigger.kill()
-  //     }
-  //   }
-  // }, [])
-
   return (
-    <div
-      ref={imageRef1}
-      className="imageTrigger"
-      style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className={styles.elevatorWrap}>
+      <img className={styles.elevator} src={historyImg01} alt="" />
       <img
-        className={`${styles.elevator} elevatorTrigger1`}
-        src={mockImg}
+        className={`${styles.elevator} elevatorTrigger`}
+        src={historyImg02}
         alt=""
       />
       <img
         className={`${styles.elevator} elevatorTrigger`}
-        src={mockImg}
-        alt=""
-      />
-      <img
-        className={`${styles.elevator} elevatorTrigger`}
-        src={mockImg}
+        src={historyImg03}
         alt=""
       />
     </div>
