8e63d01401976012a6ef26034a4923144fab450e
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-06-01 18:52:57 +0300

    check animated

---
+++ src/pages/About/components/History/components/Subtitle/Subtitle copy.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+
+import styles from './styles.module.scss'
+
+gsap.registerPlugin(ScrollTrigger)
+
+type SubtitleProps = {
+  children: string
+}
+
+export const Subtitle: React.FC<SubtitleProps> = ({ children }) => {
+  const subtitleRef = useRef<HTMLSpanElement>(null)
+  const subtitleWrapRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const subtitle = subtitleRef.current
+    const subtitleWrap = subtitleWrapRef.current
+
+    if (subtitleWrap && subtitle) {
+      gsap.to(subtitle, {
+        position: 'fixed',
+        left: subtitle?.style.left,
+        scrollTrigger: {
+          trigger: subtitleWrap,
+          start: 'top-=350vh top',
+          end: 'bottom bottom',
+          scrub: true,
+        },
+        onComplete: () => {
+          subtitle.style.position = 'absolute'
+          subtitle.style.left = '0px'
+        },
+      })
+    }
+  }, [])
+
+  return (
+    <div className={styles.historySubtitleWrap} ref={subtitleWrapRef}>
+      <span className={styles.historySubtitle} ref={subtitleRef}>
+        {children}
+      </span>
+    </div>
+  )
+}
