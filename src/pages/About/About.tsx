00a73cdf800b672cb876ff9002b0776c9fa0db28
raAndrievskiy <81430070+raAndrievskiy@users.noreply.github.com>
2023-06-02 14:39:38 +0300

    check animated

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { Info, Statistics, History } from './components'
 
 export const About: React.FC = () => {
+  const navigate = useNavigate()
+
+  const refreshPage = () => {
+    navigate(0)
+  }
+
+  refreshPage()
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
